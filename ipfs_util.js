9b6914ba4aa9efd43c235c896f7ea63df3f5c685
pieterdekker <pieter.dekker20@gmail.com>
2019-02-26 19:11:31 +0100

    updated ipfs utils for matches

--- ipfs_util.js
+++ ipfs_util.js
@@ -1,12 +1,41 @@
 let Ipfs = require('ipfs');
 let pubsub = require('pubsub-js');
 let crypto = require('crypto');
+let chalk = require('chalk');
+let keyUtils = require('./key_utils');
+
+let account = "";
 
 /** @type {boolean} Indicates wether an ipfs node is running */
 // let IPFS_UP = false;
 
+pubsub.subscribe("IPFS", (message, data) => {
+	// console.log(chalk.blue("IPFS message: ") + message + " " + data);
+	if (data === 'NODE_READY') {
+		// console.log(chalk.blue("Ipfs node ready"));
+	}
+});
+
 /** @type {Ipfs} ipfs_node */
 let ipfs_node = new Ipfs({repo: './ipfs'});
+
+ipfs_node.on('init', () => {
+	console.log(chalk.magenta("INFO: IPFS init"));
+});
+
+ipfs_node.on('start', () => {
+	console.log(chalk.magenta("INFO: IPFS start"));
+});
+
+let isReady = false;
+ipfs_node.on('ready', () => {
+	isReady = true;
+	console.log(chalk.magenta("INFO: IPFS ready"));
+});
+
+ipfs_node.on('stop', () => {
+	console.log(chalk.magenta("INFO: IPFS stop"));
+});
 
 let publicKeys = {};
 
@@ -18,7 +47,11 @@ function addPublicKey(account, key) {
 }
 
 function getPublicKey(account) {
-	if (!(account in publicKeys)) throw new Error(account + " does not yet have a public key registered");
+	if (!(account in publicKeys)) {
+		console.log(publicKeys);
+		console.log(account);
+		throw new Error(account + " does not yet have a public key registered");
+	}
 	return publicKeys[account];
 }
 
@@ -28,6 +61,29 @@ function setPrivateKey(key) {
 
 function getPrivateKey() {
 	return privateKey;
+}
+
+function setAccount(_account) {
+	account = _account;
+	let keyPair;
+	if (!keyUtils.keyPairExists(account)) {
+		keyPair = keyUtils.newKeyPairForAccount(account);
+	} else {
+		keyPair = keyUtils.getKeyPairForAccount(account);
+	}
+	addPublicKey(account, keyPair.public);
+	setPrivateKey(keyPair.private);
+}
+
+function getKeyPair(_account) {
+	if (account !== _account) {
+		throw new Error(_account + " is not set as account for ipfs utils");
+	}
+	let keyPair = {};
+	keyPair.public = getPublicKey(_account);
+	keyPair.private = getPrivateKey(_account);
+
+	return keyPair;
 }
 
 /**
@@ -101,30 +157,28 @@ async function retrieveObject(ipfs, ipfsPath) {
 /**
  * @typedef {Object} Bid
  * @property {Number} type
- * @property {Number} unit_price
  * @property {Number} volume
+ * @property {Number} unit_price
  * @property {Number} expires
  * @property {string} owner
  */
 
 /**
  * @typedef {Object} TradeAgreement
- * @property {string} offerer
- * @property {string} demander
+ * @property {string} offer_owner
+ * @property {string} demand_owner
  * @property {string} offer_file
- * @property {Number} offer_id
  * @property {string} demand_file
- * @property {Number} demand_id
- * @property {Number} unit_price
  * @property {Number} volume
+ * @property {Number} unit_price
  * @property {Number} end_date
  */
 
 /**
  * @typedef {Object} IpfsBid
  * @property {Number} type
- * @property {Number} unit_price
  * @property {Number} volume
+ * @property {Number} unit_price
  * @property {Number} expires
  * @property {string} owner
  * @property {string} signature
@@ -132,14 +186,12 @@ async function retrieveObject(ipfs, ipfsPath) {
 
 /**
  * @typedef {Object} IpfsTradeAgreement
- * @property {string} offerer
- * @property {string} demander
+ * @property {string} offerOwner
+ * @property {string} demand_owner
  * @property {string} offer_file
- * @property {Number} offer_id
  * @property {string} demand_file
- * @property {Number} demand_id
- * @property {Number} unit_price
  * @property {Number} volume
+ * @property {Number} unit_price
  * @property {Number} end_date
  * @property {string} leader_signature
  */
@@ -152,8 +204,8 @@ async function retrieveObject(ipfs, ipfsPath) {
 function bidToSignatureString(bid) {
 	return "" +
 		JSON.stringify(bid.type) +
-		JSON.stringify(bid.unit_price) +
 		JSON.stringify(bid.volume) +
+		JSON.stringify(bid.unit_price) +
 		JSON.stringify(bid.expires) +
 		JSON.stringify(bid.owner);
 }
@@ -165,14 +217,12 @@ function bidToSignatureString(bid) {
  */
 function tradeAgreementToSignatureString(tradeAgreement) {
 	return "" +
-		JSON.stringify(tradeAgreement.offerer) +
-		JSON.stringify(tradeAgreement.demander) +
+		JSON.stringify(tradeAgreement.offer_owner) +
+		JSON.stringify(tradeAgreement.demand_owner) +
 		JSON.stringify(tradeAgreement.offer_file) +
-		JSON.stringify(tradeAgreement.offer_id) +
 		JSON.stringify(tradeAgreement.demand_file) +
-		JSON.stringify(tradeAgreement.demand_id) +
-		JSON.stringify(tradeAgreement.unit_price) +
 		JSON.stringify(tradeAgreement.volume) +
+		JSON.stringify(tradeAgreement.unit_price) +
 		JSON.stringify(tradeAgreement.end_date);
 }
 
@@ -235,7 +285,7 @@ function verifyTradeAgreementSignature(tradeAgreementFromIPFS, publicKey) {
  */
 async function storeBid(ipfs, bid, privateKey, path="") {
 	bid['signature'] = createBidSignature(bid, privateKey);
-	return (await storeObject(ipfs, bid, path))[0];
+	return (await storeObject(ipfs, bid, path))[0].path;
 }
 
 /**
@@ -283,23 +333,31 @@ async function retrieveTradeAgreement(ipfs, ipfsPath) {
  */
 async function getNode() {
 	return new Promise((resolve, reject) => {
+		if (isReady) resolve(ipfs_node);
 		ipfs_node.on('ready', () => {
 			pubsub.publish('IPFS', "NODE_READY");
+			isReady = true;
 			resolve(ipfs_node);
 		});
 	});
 }
 
+setAccount("0x47681d90A3B1B044980c39ed1e32D160a8043Ceb");
+
 module.exports = {
 	getNode: getNode,
+	// stopNode: stopNode,
 	addPublicKey: addPublicKey,
 	getPublicKey: getPublicKey,
 	setPrivateKey: setPrivateKey,
 	getPrivateKey: getPrivateKey,
+	setAccount: setAccount,
+	getKeyPair: getKeyPair,
 	storeObject: storeObject,
 	retrieveObject: retrieveObject,
 	storeBid: storeBid,
-	storeTradeAgreement,
+	storeTradeAgreement: storeTradeAgreement,
 	retrieveBid: retrieveBid,
-	retrieveTradeAgreement
+	retrieveTradeAgreement: retrieveTradeAgreement,
+	verifyTradeAgreementSignature: verifyTradeAgreementSignature
 };
