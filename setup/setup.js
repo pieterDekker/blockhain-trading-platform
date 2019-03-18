895af34815ab72d88a965897a5a9318aef370a04
pieterdekker <pieter.dekker20@gmail.com>
2019-03-18 22:15:50 +0100

    added setup file for setting up whole network

--- setup/setup.js
+++ setup/setup.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
+const solc = require('solc');
+
 const nodeUtils = require('../node_utils');
 const testUtils = require('../test_util');
-const fs = require('fs');
+
 const node = nodeUtils.getNode();
 
 const testAccount = "0x47681d90A3B1B044980c39ed1e32D160a8043Ceb";
@@ -8,36 +11,27 @@ const testPassword = "testaccount";
 let gasAmount = 100000000000;
 
 /**
- * @typedef {Object} IncludeComposite
+ * @typedef {Object} Deployed
  * @property {String} fileName
  * @property {String} file
  * @property {String} contract
  */
 
-/**
- * @param fileName
- * @param includeComposites
- */
+function getContract(abi) {
+	return new node.eth.Contract(abi);
+}
 
-async function getContract(fileName, includeComposites) {
-	let file = fs.readFileSync('../contracts/' + fileName).toString();
-	let compiled;
-	let arguments = [];
+async function deploy(contract, byteCode, arguments, account, gasAmount) {
+	return await contract.deploy({data: byteCode, arguments: arguments}).send({from: account, gas: gasAmount});
+}
 
-	if (includes.length < 1) {
-		compiled = testUtils.compileContractFromFile(file);
-	} else {
-		let includes = {};
-		for (let includeComposite of includeComposites) {
-			includes[includeComposite.fileName] = includeComposite.file;
-			arguments.push(includeComposite.contract._address);
-		}
-		compiled = testUtils.compileContractFromFileWithIncludes(file, includes);
-	}
+async function deployCompiled(compiled, account, gasAmount, arguments = []) {
+	let contract = getContract(JSON.parse(compiled.interface));
+	return await deploy(contract, "0x" + compiled.bytecode, arguments, account, gasAmount);
+}
 
-	
-
-
+function getCompiledByName(compilationOutput, name) {
+	return compilationOutput.contracts[testUtils.upperToLowerCamelCase(name) + ".sol:" + name];
 }
 
 async function deployContracts(account, passPhrase, gasAmount) {
@@ -45,38 +39,48 @@ async function deployContracts(account, passPhrase, gasAmount) {
 
 	node.miner.start(1);
 
-	let marketplaceFile = fs.readFileSync('../contracts/marketplace.sol').toString();
-	let marketplaceCompiled = testUtils.compileContractFromFile(marketplaceFile);
-	let marketplaceContract =
-		await (new node.eth.Contract(marketplaceCompiled.abi))
-			.deploy({data: marketplaceCompiled.bytecode})
-			.send({from: account, gas: gasAmount});
+	let contractFilenames = fs.readdirSync('../contracts').filter((file) => file.endsWith(".sol"));
+	let fullFiles = {};
+	for (let fileName of contractFilenames) {
+		fullFiles[fileName] = fs.readFileSync('../contracts/' + fileName).toString();
+	}
 
-	let paymentAgreementsFile = fs.readFileSync('../contracts/paymentAgreements.sol').toString();
-	let paymentAgreementsCompiled = testUtils.compileContractFromFile(paymentAgreementsFile);
-	let paymentAgreementsContract =
-		await (new node.eth.Contract(paymentAgreementsCompiled.abi))
-			.deploy({data: paymentAgreementsCompiled.bytecode})
-			.send({from: account, gas: gasAmount});
+	let compiled = solc.compile({sources: fullFiles}, 1);
+	if (Array.isArray(compiled.errors) && compiled.errors.length > 0) {
+		console.log("There were errors in compilation of the contracts");
+		compiled.errors.map((err) => console.log(err));
+	}
 
-	let tradeAgreementsFile = fs.readFileSync('../contracts/tradeAgreements.sol').toString();
-	let tradeAgreementsCompiled = testUtils.compileContractFromFileWithIncludes({'tradeAgreements.sol': tradeAgreementsFile}, {'paymentAgreements.sol': paymentAgreementsFile});
-	let tradeAgreementsContract =
-		await (new node.eth.Contract(tradeAgreementsCompiled.abi))
-			.deploy({data: tradeAgreementsCompiled.bytecode, arguments: [paymentAgreementsContract._address]})
-			.send({from: account, gas: gasAmount});
+	let marketplace = await deployCompiled(getCompiledByName(compiled, 'Marketplace'), account, gasAmount);
+	let paymentAgreements = await deployCompiled(getCompiledByName(compiled, 'PaymentAgreements'), account, gasAmount);
+	let tradeAgreements = await deployCompiled(getCompiledByName(compiled, 'TradeAgreements'), account, gasAmount, [paymentAgreements._address]);
+	let matches = await deployCompiled(getCompiledByName(compiled, 'Matches'), account, gasAmount, [tradeAgreements._address]);
 
-	return
+	return {
+		marketplace: marketplace,
+		paymentAgreements: paymentAgreements,
+		tradeAgreements: tradeAgreements,
+		matches: matches
+	};
 }
 
 deployContracts(testAccount, testPassword, gasAmount)
 	.then((contracts) =>{
 		console.log("contracts deployed");
+		console.log(Object.keys(contracts));
+		for (let contract in contracts) {
+			console.log(contract + " is at " + contracts[contract]._address);
+		}
 		node.miner.stop();
 		process.exit(0);
 	})
 	.catch((error) => {
 		console.log("Error in deployContracts: " + error.message);
+		console.log(error);
 		node.miner.stop();
 		process.exit(-1);
 	});
+
+module.exports = {
+	deployContracts: deployContracts
+};
