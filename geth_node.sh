deaffce51bf1f56383813d7f46721f02be3129b4
pieterdekker <pieter.dekker20@gmail.com>
2019-05-22 23:16:32 +0200

    cleaned up geth run scripts and updated gitignore

--- geth_node.sh
+++ geth_node.sh
@@ -20,41 +20,48 @@ arg=$1
 
 if [[ "$arg" == "init" ]]
 then
-	echo "initializing the mutant chain..."
+	echo "initializing the blockchain..."
 	echo "node name is: $HOST_NAME"
 
 	#init the chain, create genesisblock etc.
-	./geth --identity $HOST_NAME --gasprice "0" --datadir "./private_chain" --port "30303" --nodiscover \
+	./geth --identity $HOST_NAME --gasprice 0 --datadir "./private_chain" --ethash.dagdir "./ethash" --port "30303" --nodiscover \
 	--networkid 230594 init $PWD"/chain_init_data/genesis_json" 2>>node_error | tee -a node_output
 elif [[ "$arg" == "reinit" ]]
 then
-    echo "removing old chain"
+    echo "removing old blockchain"
     rm -r $PWD/private_chain/
     rm $PWD/node_error $PWD/node_output
-	echo "reinitializing the chain (as mutant chain)..."
+	echo "reinitializing the blockchain ..."
 	echo "node name is: $HOST_NAME"
+
 	#init the chain, create genesisblock etc.
-	./geth --identity $HOST_NAME --gasprice "0" --datadir "./private_chain" --port "30303" --nodiscover	--fakepow \
+	./geth --identity $HOST_NAME --gasprice 0 --datadir "./private_chain" --ethash.dagdir "./ethash" --port "30303" --nodiscover \
 	--ws --wsport 8546 --wsorigins "*" --wsapi "db,eth,net,web3,miner,personal" \
 	--syncmode "light" --verbosity 5 --minerthreads 1 --networkid 230594 init $PWD"/chain_init_data/genesis_json" 2>>node_error | tee -a node_output
 	cp $PWD/chain_init_data/UTC--2018-06-26T03-59-03.509883478Z--47681d90a3b1b044980c39ed1e32d160a8043ceb $PWD/private_chain/keystore/
 elif [[ "$arg" == "run" ]]
 then
-	echo "running the mutant chain and starting a console..."
+	echo "running a geth node and starting a console..."
 	echo "node name is: $HOST_NAME"
 
 	#run the node and start a console to interact with it
-	./geth --identity $HOST_NAME --gasprice "0" --datadir "./private_chain" --port "30303" --nodiscover \
+	./geth --identity $HOST_NAME --gasprice 0 --datadir "./private_chain" --ethash.dagdir "./ethash" --port "30303" --nodiscover \
 	--ws --wsport 8546 --wsorigins "*" --wsapi "db,eth,net,web3,miner,personal" --networkid 230594 \
 	--verbosity 5 --minerthreads 1 console 2>>node_error
-else
-	echo "Usage: private_chain [init|reinit|run]"
+elif [[ "$arg" == "detached" ]]
+then
+	echo "running a geth node in the background..."
+	echo "node name is: $HOST_NAME"
 
-	echo "	init: create and initialize a new chain"
-	echo "	init: remove a previously created chain, reinitialize a new chain and continue with old account"
-	echo "	run: run an already initialized chain"
-	echo "running now"
-	./geth --identity $HOST_NAME --gasprice "0" --datadir "./private_chain" --port "30303" --nodiscover \
+	#run the node and start a console to interact with it
+	./geth --identity $HOST_NAME --gasprice 0 --datadir "./private_chain" --ethash.dagdir "./ethash" --port "30303" --nodiscover \
 	--ws --wsport 8546 --wsorigins "*" --wsapi "db,eth,net,web3,miner,personal" --networkid 230594 \
-	--verbosity 5 --minerthreads 1 console 2>>node_error
+	--verbosity 5 --minerthreads 1 2>>node_error
+else
+	echo "Usage: private_chain [init|reinit|run|detached]"
+
+	echo "	init: create and initialize a new blockchain"
+	echo "	init: remove a previously created blockchain, initialize a new blockchain"
+	echo "	run: run a geth node and starting an attached console, requires an initialized blockchain"
+	echo "  detached: run a geth node in the background, requires an initialized blockchain"
 fi
