350d849749389d947122e18959b466d861539806
pieterdekker <pieter.dekker20@gmail.com>
2020-04-27 23:54:26 +0200

    remove the marketplace migration for now

--- migrations/1586247178_deploy_marketplace.js
+++
@@ -1,6 +0,0 @@
-// const Marketplace = artifacts.require('Marketplace')
-
-module.exports = function(_deployer) {
-  // Use deployer to state migration tasks.
-  // _deployer.deploy(Marketplace)
-};
